c04cd1b82d888c37cd23bf8845d5ce7d683e1165
Laura reed <pilesoflaura@gmail.com>
2020-02-15 10:24:20 -0700

    If run this code will have first event added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let busImg = document.querySelector('img')
+busImg.addEventListener('click', () => {
+    busImg.style.transform = "scale(0.5)"
+    busImg.style.transition = "transform 1s";
+})
