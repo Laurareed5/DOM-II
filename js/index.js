fbbc508b4c92867001bc422d562e15c08b21f41b
Laura reed <pilesoflaura@gmail.com>
2020-02-15 10:37:03 -0700

    If run, this code will have second event added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 // Your code goes here
 let busImg = document.querySelector('img')
 busImg.addEventListener('click', () => {
-    busImg.style.transform = "scale(0.5)"
+    busImg.style.transform = "scale(0)"
     busImg.style.transition = "transform 1s";
+})
+
+const coLor = document.querySelector('p')
+coLor.addEventListener('dblclick', () => {
+    coLor.style.color = "yellow";
 })
